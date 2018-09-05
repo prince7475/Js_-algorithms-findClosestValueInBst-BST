9fcb17cff1f3ecc4c8490aee2159491940510ce0
Prince <prince@Princes-MacBook-Pro.local>
2018-09-05 12:15:56 -0400

    Problem solved

--- findClosestValueInBst.js
+++ findClosestValueInBst.js
@@ -1,4 +1,20 @@
-function findClosestValueInBst(array,target){
-    return 8
+function findClosestValueInBst(tree,target){
+    let currentNode = tree.test
+    let closest = Infinity
+    while(currentNode){
+        let value = currentNode.value
+        if (Math.abs(target - closest) > Math.abs(target - value)){
+            closest = value
+        }
+        if (value > target){
+            currentNode = currentNode.left
+        }else if(value < target){
+            currentNode = currentNode.right
+        }else {
+            return closest
+        }
+    }
+    return closest
+
 }
 exports.findClosestValueInBst = findClosestValueInBst
